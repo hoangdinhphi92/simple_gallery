5ae36f04ab225651660724c06d4816eb2044ad5d
songiang95 <songiang1995@gmail.com>
2025-02-28 11:42:40 +0700

    update.

--- lib/src/detail/detail_image_screen.dart
+++ lib/src/detail/detail_image_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:simple_gallery/src/detail/notifier/detail_value_notifier.dart';
 
 class DetailImageScreen extends StatefulWidget {
   final String imagePath;
@@ -10,8 +12,36 @@ class DetailImageScreen extends StatefulWidget {
 }
 
 class _DetailImageScreenState extends State<DetailImageScreen> {
+  late final DetailValueNotifier _detailValueNotifier;
+  late final FileImage _imageProvider = FileImage(File(widget.imagePath));
+
+  @override
+  void initState() {
+    super.initState();
+    _detailValueNotifier = DetailValueNotifier(widget.imagePath);
+    _getImageSize();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        _detailValueNotifier.onViewSizeChanged(constraints.biggest);
+
+        return Image(image: _imageProvider, fit: BoxFit.contain);
+      },
+    );
+  }
+
+  void _getImageSize() {
+    final imageStream = _imageProvider.resolve(ImageConfiguration());
+
+    final listener = ImageStreamListener((info, _) {
+      _detailValueNotifier.setImageSize(
+        Size(info.image.width.toDouble(), info.image.height.toDouble()),
+      );
+    });
+
+    imageStream.addListener(listener);
   }
 }
