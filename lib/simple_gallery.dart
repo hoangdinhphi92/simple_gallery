a3eac72bb755a227944358bca4f3af438c8671c0
songiang95 <songiang1995@gmail.com>
2025-03-13 11:31:47 +0700

    update.

--- lib/simple_gallery.dart
+++ lib/simple_gallery.dart
@@ -1,4 +1,4 @@
 export 'package:simple_gallery/src/gallery/simple_gallery.dart';
 export 'package:simple_gallery/src/detail/detail_decoration.dart';
-export 'package:simple_gallery/src/detail/zoom/zoomable_notification.dart';
 export 'package:simple_gallery/src/detail/zoom/zoomable_preview.dart';
+export 'package:simple_gallery/src/detail/zoom/zoomable_notifier.dart';
